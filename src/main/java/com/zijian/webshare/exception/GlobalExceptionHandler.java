348541249bf16f276124ea7e61177dd5beaabac4
zijian-z <zijian-zhang@outlook.com>
2021-01-20 14:36:44 +0800

    添加对各种资源不存在的处理

--- src/main/java/com/zijian/webshare/exception/GlobalExceptionHandler.java
+++ src/main/java/com/zijian/webshare/exception/GlobalExceptionHandler.java
@@ -8,6 +8,8 @@ import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import javax.validation.ConstraintViolationException;
+
 @RestControllerAdvice
 public class GlobalExceptionHandler {
     private static final Logger log = LoggerFactory.getLogger(GlobalExceptionHandler.class);
@@ -18,6 +20,16 @@ public class GlobalExceptionHandler {
      */
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<String> handleValidException(MethodArgumentNotValidException e) {
-        return new ResponseEntity<>(e.getBindingResult().getFieldError().getDefaultMessage(), HttpStatus.OK);
+        return new ResponseEntity<>(e.getMessage(), HttpStatus.OK);
+    }
+
+    /**
+     * 处理资源不存在的错误（用户不存在、链接不存在）
+     * @param e
+     * @return
+     */
+    @ExceptionHandler(ResourceEmptyException.class)
+    public ResponseEntity<String> handleEmptyException(ResourceEmptyException e) {
+        return new ResponseEntity<>(e.getMessage(), HttpStatus.OK);
     }
 }
