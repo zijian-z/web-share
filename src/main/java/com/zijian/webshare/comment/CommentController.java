ea1ddb89e7dca975120aced6421cd62bec6f4da5
zijian-z <zijian-zhang@outlook.com>
2021-01-21 16:42:55 +0800

    添加评论通知和@通知

--- src/main/java/com/zijian/webshare/comment/CommentController.java
+++ src/main/java/com/zijian/webshare/comment/CommentController.java
@@ -3,12 +3,12 @@ package com.zijian.webshare.comment;
 import com.zijian.webshare.exception.ResourceEmptyException;
 import com.zijian.webshare.link.Link;
 import com.zijian.webshare.link.LinkService;
+import com.zijian.webshare.notify.NotifyService;
+import com.zijian.webshare.notify.NotifyType;
 import com.zijian.webshare.user.User;
 import com.zijian.webshare.user.UserService;
+import com.zijian.webshare.util.CommentUtil;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -23,18 +23,18 @@ public class CommentController {
     private final CommentService commentService;
     private final LinkService linkService;
     private final HttpSession httpSession;
-    private final UserService userService;
     private final HashMap<String, User> map;
+    private final NotifyService notifyService;
 
     @Autowired
     public CommentController(CommentService commentService, LinkService linkService,
-                             UserService userService, HttpSession httpSession,
-                             HashMap<String, User> map) {
+                             HttpSession httpSession, HashMap<String, User> map,
+                             NotifyService notifyService) {
         this.commentService = commentService;
         this.linkService = linkService;
-        this.userService = userService;
         this.httpSession = httpSession;
         this.map = map;
+        this.notifyService = notifyService;
     }
 
     @PostMapping("/")
@@ -44,6 +44,14 @@ public class CommentController {
             User user = map.get(httpSession.getAttribute("user").toString());
             Comment savedComment = commentService.save(
                     commentVO.getContent(), user, optional.get());
+
+            // 保存评论通知
+            notifyService.save(user, savedComment.getLink().getUser(), savedComment.getLink(), savedComment);
+            // 保存@通知
+            if (savedComment.getContent().contains("@")) {
+                notifyService.save(user, CommentUtil.findAtUsers(savedComment.getContent()), savedComment.getLink(), savedComment);
+            }
+
             return new ResponseEntity<>(savedComment, HttpStatus.OK);
         } else {
             throw new ResourceEmptyException("link.empty");
