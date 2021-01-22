3c9fc23c344d390db351c3d355d1587d32497ea6
zijian-z <zijian-zhang@outlook.com>
2021-01-22 14:30:49 +0800

    测试

--- src/test/java/com/zijian/webshare/WebShareApplicationTests.java
+++ src/test/java/com/zijian/webshare/WebShareApplicationTests.java
@@ -1,15 +1,21 @@
 package com.zijian.webshare;
 
+import com.zijian.webshare.comment.CommentService;
+import com.zijian.webshare.exception.GlobalExceptionHandler;
 import com.zijian.webshare.link.Link;
 import com.zijian.webshare.link.LinkRepository;
 import com.zijian.webshare.link.LinkService;
 import com.zijian.webshare.user.User;
 import com.zijian.webshare.user.UserService;
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.test.web.servlet.MvcResult;
 
 import java.time.LocalDateTime;
@@ -17,10 +23,16 @@ import java.time.LocalTime;
 
 @SpringBootTest
 class WebShareApplicationTests {
+    private static final Logger log = LoggerFactory.getLogger(WebShareApplicationTests.class);
+
     @Autowired
     UserService userService;
     @Autowired
     LinkService linkService;
+    @Autowired
+    CommentService commentService;
+    @Autowired
+    StringRedisTemplate template;
 
     @Test
     void contextLoads() {
@@ -28,18 +40,19 @@ class WebShareApplicationTests {
 
     @Test
     void register() {
-        userService.register("zhangsan", "zhangsan");
-    }
-
-    @Test
-    void login() {
+//        userService.register("zhangsan", "zhangsan");
     }
 
     @Test
     void addLink() {
-        User user = userService.findByUsername("zhangsan").get();
-        System.out.println(user);
-        linkService.addLink(new Link("https://www.baidu.com", System.currentTimeMillis(), user));
-        System.out.println(linkService.findAllByUserId(user.getId(), PageRequest.of(1, 20)).getTotalElements());
+//        User user = userService.findByUsername("zhangsan").get();
+//        linkService.save(new Link("https://www.baidu.com", System.currentTimeMillis(), user, null));
+    }
+
+    @Test
+    void addComment() {
+//        User user = userService.findByUsername("zhangsan").get();
+//        Page<Link> links = linkService.findAllByUserId(user.getId(), PageRequest.of(0, 20));
+//        commentService.save("测试评论", user, null, links.getContent().get(0));
     }
 }
